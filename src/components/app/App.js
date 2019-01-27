936d40a035e46a1e718706791c9332d32456d5bd
Raymi Saldomando <raymi@able.co>
2019-01-27 13:04:50 -0500

    Setup dev server port, styled components and component files

---
+++ src/components/app/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './styles';
+
+const App = () => {
+  return <Styled.App>React Lessons</Styled.App>;
+};
+
+export default App;
---
+++ src/components/app/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const App = styled.div`
+  background: black;
+  color: white;
+  position: relative;
+  font-size: 14em;
+`;
